3d046abdc709fdc193e36e92a2dfab0500172826
Talles Pereira de Brito <tallespereira55@gmail.com>
2023-06-06 19:20:11 -0300

    (REQ20) Requisito concluído mas não aprovado pelo evaluator pois pede 45% de testes (que farei na finalização da tela de receitas). Cria os botões que serão utilizados para filtrar as receitas renderizadas na tela

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,35 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Card } from '../components/Card';
 import Header from '../components/Header';
+import { FiltterButton } from '../components/FilterButton';
 
 // Explicação da aplicação requisito 19: a página de receitas tem duas possíves rotas, a /drinks e a /meals,
 // a página nos trás informações sobre receitas e depende de sua rota para mostrar um devido tipo, no caso entre comidas e bebidas.
 
 export default function Recipes() {
   // Aqui capturamos a rota que está sendo utilizada para se acessar a página.
-  const currentPath = window.location.pathname;
+  const history = useHistory();
+  const currentPath = history.location.pathname;
   const isMealRoute = currentPath === '/meals';
 
   const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const MN2 = 5;
 
   // Esse use effect faz um fetch assim que a página é montada de acordo com a rota utilizada e capturada no código sinalizado acima.
   useEffect(() => {
-    const n = 12;
+    const MN1 = 12;
     if (isMealRoute) {
       fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
         .then((response) => response.json())
         .then(({ meals }) => {
-          const treatedData = meals.slice(0, n);
-          setRecipes(treatedData);
-          console.log(recipes);
+          const treatedDataForRecipes = meals.slice(0, MN1);
+          setRecipes(treatedDataForRecipes);
+        });
+      fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+        .then((response) => response.json())
+        .then(({ meals }) => {
+          setCategories(meals);
+          console.log(meals);
         });
     } else {
       fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
         .then((response) => response.json())
         .then(({ drinks }) => {
-          const treatedData = drinks.slice(0, n);
-          setRecipes(treatedData);
-          console.log(recipes);
+          const treatedDataForRecipes = drinks.slice(0, MN1);
+          setRecipes(treatedDataForRecipes);
+          // console.log(drinks);
+        });
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+        .then((response) => response.json())
+        .then(({ drinks }) => {
+          setCategories(drinks);
+          console.log(drinks);
         });
     }
   }, []);
@@ -38,6 +54,21 @@ export default function Recipes() {
     <div>
       <Header />
       <main>
+        { isMealRoute ? categories
+          .slice(0, MN2)
+          .map(({ strCategory }, i) => (
+            <FiltterButton
+              key={ i }
+              category={ strCategory }
+            />
+          )) : categories
+          .slice(0, MN2)
+          .map(({ strCategory }, i) => (
+            <FiltterButton
+              key={ i }
+              category={ strCategory }
+            />
+          ))}
         {/* Aqui Fazemos uso do operador ternario e do componente ../components/Card criado para essa aplicação,
       dependendo de qual rota estiver sendo utilizada a página renderiza os 12
       primeiros itens da lista que de receitas que a devida API nos retorna.
@@ -59,64 +90,5 @@ export default function Recipes() {
         ))}
       </main>
     </div>
-  );
-}
-import React, { useEffect, useState } from 'react';
-import { Card } from '../components/Card';
-
-// Explicação da aplicação requisito 19: a página de receitas tem duas possíves rotas, a /drinks e a /meals,
-// a página nos trás informações sobre receitas e depende de sua rota para mostrar um devido tipo, no caso entre comidas e bebidas.
-
-export default function Recipes() {
-  // Aqui capturamos a rota que está sendo utilizada para se acessar a página.
-  const currentPath = window.location.pathname;
-  const isMealRoute = currentPath === '/meals';
-
-  const [recipes, setRecipes] = useState([]);
-
-  // Esse use effect faz um fetch assim que a página é montada de acordo com a rota utilizada e capturada no código sinalizado acima.
-  useEffect(() => {
-    const n = 12;
-    if (isMealRoute) {
-      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-        .then((response) => response.json())
-        .then(({ meals }) => {
-          const treatedData = meals.slice(0, n);
-          setRecipes(treatedData);
-          console.log(recipes);
-        });
-    } else {
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-        .then((response) => response.json())
-        .then(({ drinks }) => {
-          const treatedData = drinks.slice(0, n);
-          setRecipes(treatedData);
-          console.log(recipes);
-        });
-    }
-  }, []);
-
-  return (
-    <main>
-      {/* Aqui Fazemos uso do operador ternario e do componente ../components/Card criado para essa aplicação,
-      dependendo de qual rota estiver sendo utilizada a página renderiza os 12
-      primeiros itens da lista que de receitas que a devida API nos retorna.
-      (O requisito pede que sejam renderizados apenas os primeiros 12 itens.)  */}
-      { isMealRoute ? recipes.map((recipe, i) => (
-        <Card
-          key={ recipe.strMeal }
-          name={ recipe.strMeal }
-          imgSrc={ recipe.strMealThumb }
-          SrcIndex={ i }
-        />
-      )) : recipes.map((recipe, i) => (
-        <Card
-          key={ recipe.strDrink }
-          name={ recipe.strDrink }
-          imgSrc={ recipe.strDrinkThumb }
-          SrcIndex={ i }
-        />
-      ))}
-    </main>
   );
 }
