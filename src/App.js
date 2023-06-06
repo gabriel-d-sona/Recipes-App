854632866e4476cba6d5f81a28e65c913465cc86
Campos <pabloucf2@gmail.com>
2023-06-05 21:18:42 -0300

    (REQ7) Test OK, iniciando req8

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" />
+        <Route exact path="/meals" component={ Meals } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       </Switch>
     </div>
   );
