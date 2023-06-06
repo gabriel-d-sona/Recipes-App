60f6ce7d8403e75cce6a417a09be98508514b0c4
Nicolas Lima <nicolaslima1500@gmail.com>
2023-06-06 20:21:48 -0300

    Requisito 24 finalizado

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Drinks from './pages/Drinks';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/meals" component={ Meals } />
-        <Route path="/drinks" component={ Drinks } />
         <Route path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route path="/drinks/:drinksId" component={ RecipeDetails } />
+        <Route path="/meals/:mealsId" component={ RecipeDetails } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/meals" component={ Meals } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </div>
   );
