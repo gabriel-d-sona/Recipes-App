5424727f28358aedcb23567440b0f5848bd79402
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-06-14 13:54:28 -0300

    req 37 ao 39(terminar testes do req 39)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Drinks from './pages/Drinks';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeInProgress from './components/RecipeInProgress';
 
 function App() {
   return (
@@ -14,10 +15,12 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/meals" component={ Meals } />
-        <Route path="/drinks" component={ Drinks } />
+        <Route exact path="/drinks" component={ Drinks } />
         <Route path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+        <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
       </Switch>
     </div>
   );
