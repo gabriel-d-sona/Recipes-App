ac4c7719a4025cbb784d36ced4bbb3685e7c5a84
Nicolas Lima <nicolaslima1500@gmail.com>
2023-06-14 12:57:55 -0300

    Fazendo requisito 27

--- src/components/MealObservations.js
+++ src/components/MealObservations.js
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import filterArrays from '../services/helpers/filterArrays';
 import requestApi from '../services/helpers/requestApi';
+import 'bootstrap/dist/css/bootstrap.css';
+import './carousel.css';
 
 const endPointForDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 function MealObservations({ recipe }) {
@@ -27,6 +30,8 @@ function MealObservations({ recipe }) {
 
   const strIngredients = filterArrays(recipe[0], 'strIngredient');
   const strMeasures = filterArrays(recipe[0], 'strMeasure');
+
+  // const six = 6;
 
   return (
     <div>
@@ -97,6 +102,117 @@ function MealObservations({ recipe }) {
         data-testid="video"
       />
 
+      <h4>Recomendações:</h4>
+
+      {/* <Carousel>
+        <Carousel.Item>
+          <div className="carousel-container">
+            <div data-testid="0-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[0].strDrinkThumb }
+                alt={ drinks[0].strDrink }
+              />
+              <h4
+                data-testid="0-recommendation-card"
+              >
+                {drinks[0].strDrink}
+
+              </h4>
+            </div>
+            <div data-testid="1-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[1].strDrinkThumb }
+                alt={ drinks[1].strDrink }
+              />
+              <h4
+                data-testid="1-recommendation-card"
+              >
+                {drinks[1].strDrink}
+
+              </h4>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-container">
+            <div data-testid="2-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[2].strDrinkThumb }
+                alt={ drinks[2].strDrink }
+              />
+              <h4
+                data-testid="2-recommendation-card"
+              >
+                {drinks[2].strDrink}
+
+              </h4>
+            </div>
+            <div data-testid="3-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[3].strDrinkThumb }
+                alt={ drinks[3].strDrink }
+              />
+              <h4
+                data-testid="3-recommendation-card"
+              >
+                {drinks[3].strDrink}
+
+              </h4>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-container">
+            <div data-testid="4-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[4].strDrinkThumb }
+                alt={ drinks[4].strDrink }
+              />
+              <h4
+                data-testid="4-recommendation-card"
+              >
+                {drinks[4].strDrink}
+
+              </h4>
+            </div>
+            <div data-testid="5-recommendation-card">
+              <img
+              // className="img-carousel"
+                src={ drinks[5].strDrinkThumb }
+                alt={ drinks[5].strDrink }
+              />
+              <h4
+                data-testid="5-recommendation-card"
+              >
+                {drinks[5].strDrink}
+
+              </h4>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel> */}
+      {/* {
+        drinks.map((drink, index) => (index < six ? (
+          <div key={ index } data-testid={ `${index}-recommendation-card` }>
+            <Carousel>
+              <Carousel.Item>
+                <div className="carousel-container">
+                  <img
+                    className="img-carousel"
+                    src={ drink.strDrinkThumb }
+                    alt={ drink.strDrink }
+                  />
+                </div>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        ) : undefined))
+      } */}
     </div>
   );
 }
