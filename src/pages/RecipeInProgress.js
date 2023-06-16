c50f55d3e1268d50327872fba5a5a41f4a68efb1
Talles Pereira de Brito <tallespereira55@gmail.com>
2023-06-16 17:53:52 -0300

    (REQ43) Concluido

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function RecipeInProgress() {
+  const history = useHistory();
   const [recipeDetails, setRecipeDetails] = useState({});
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
-  const [checkedIngredients, setCheckedIngredients] = useState([]);
+  const [checkedIngredients, setCheckedIngredients] = useState([]); // Estado para os ingredientes marcados
 
   useEffect(() => {
     const fetchRecipeDetails = async () => {
@@ -14,7 +16,6 @@ function RecipeInProgress() {
       const apiUrl = isDrinkRoute
         ? `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
         : `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
-
       const response = await fetch(apiUrl);
       const data = await response.json();
       if (data.meals || data.drinks) {
@@ -23,12 +24,18 @@ function RecipeInProgress() {
       setIsLoading(false);
     };
     fetchRecipeDetails();
+    // Verifica localStorage
+    const savedProgress = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    const isDrinkRoute = location.pathname.includes('/drinks/');
+    const recipeType = isDrinkRoute ? 'drinks' : 'meals';
+    // recipe type ABENÇOADO NAO ESTAVA VERIFICADO NO LOCALSTORAGE
+    if (savedProgress && savedProgress[recipeType] && savedProgress[recipeType][id]) {
+      setCheckedIngredients(savedProgress[recipeType][id]);
+    // correção do saveProgress[recipeType][id] para acessar os ingredientes no localstorage
+    }
   }, [id, location]);
-
   if (isLoading) return <div>Loading...</div>;
-
   const MAX_INGREDIENTS = 50;
-
   const getIngredients = (recipe) => {
     const ingredients = [];
     for (let index = 1; index <= MAX_INGREDIENTS; index += 1) {
@@ -49,9 +56,31 @@ function RecipeInProgress() {
       newCheckedIngredients.push(index);
     }
     setCheckedIngredients(newCheckedIngredients);
+    // Atualiza o localStorage
+    const savedProgress = JSON.parse(
+      localStorage.getItem('inProgressRecipes'),
+    ) || { drinks: {}, meals: {} };
+    const isDrinkRoute = location.pathname.includes('/drinks/');
+    const recipeType = isDrinkRoute ? 'drinks' : 'meals';
+    localStorage.setItem(
+      'inProgressRecipes',
+      JSON.stringify({
+        ...savedProgress,
+        [recipeType]: {
+          ...savedProgress[recipeType],
+          [id]: newCheckedIngredients,
+        },
+      }),
+    );
   };
 
+  function handleMadeRecipe() {
+    history.push('/done-recipes');
+  }
+
   const ingredients = getIngredients(recipeDetails);
+  // verifica se os ingredientes estão marcados
+  const allIngredientsChecked = ingredients.length === checkedIngredients.length;
 
   return (
     <div>
@@ -63,21 +92,18 @@ function RecipeInProgress() {
             alt="Receita a ser fe"
             data-testid="recipe-photo"
           />
-          {/* Exibe o título da receita */}
           <h2 data-testid="recipe-title">
             {recipeDetails.strMeal || recipeDetails.strDrink}
           </h2>
-          {/* Exibe a categoria da receita ou se é alcoólico em caso de bebida */}
           <h2 data-testid="recipe-category">
             {recipeDetails.strCategory || recipeDetails.strAlcoholic}
           </h2>
-
-          {/* Exibe a lista de ingredientes com checkboxes */}
           <h2>Ingredientes da receita</h2>
           <ul>
             {ingredients.map((ingredient, index) => (
               <li key={ index }>
                 <label
+                  htmlFor="checkbox"
                   data-testid={ `${index}-ingredient-step` }
                   style={
                     checkedIngredients.includes(index)
@@ -87,6 +113,8 @@ function RecipeInProgress() {
                 >
                   <input
                     type="checkbox"
+                    name="checkbox"
+                    checked={ checkedIngredients.includes(index) }
                     onChange={ () => handleCheckIngredient(index) }
                   />
                   {ingredient}
@@ -94,12 +122,17 @@ function RecipeInProgress() {
               </li>
             ))}
           </ul>
-
           <h2>Instruções</h2>
           <p data-testid="instructions">{recipeDetails.strInstructions}</p>
           <button data-testid="share-btn">Compartilhar</button>
           <button data-testid="favorite-btn">Favoritar</button>
-          <button data-testid="finish-recipe-btn">Finalizar Receita</button>
+          <button
+            onClick={ () => handleMadeRecipe() }
+            data-testid="finish-recipe-btn"
+            disabled={ !allIngredientsChecked }
+          >
+            Sua receita esta pronta!
+          </button>
         </>
       ) : (
         <p>Receita não encontrada</p>
@@ -107,5 +140,4 @@ function RecipeInProgress() {
     </div>
   );
 }
-
 export default RecipeInProgress;
