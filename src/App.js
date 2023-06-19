ea92c6875caf4e95c637b4d06e227adc4ab42e0c
Talles Pereira de Brito <tallespereira55@gmail.com>
2023-06-19 18:12:56 -0300

    tentando resolver problema de lint

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import FavoriteRecipes from './pages/FavoriteRecipes';
 import RecipeDetails from './pages/RecipeDetails';
 import Recipes from './pages/Recipes';
 import RecipeInProgress from './pages/RecipeInProgress';
-import { recipeDetails } from './pages/RecipeDetails';
 
 function App() {
   return (
@@ -17,14 +16,14 @@ function App() {
     <Switch>
       <Route exact path="/meals" component={ Recipes } />
       <Route exact path="/drinks" component={ Recipes } />
+      <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+      <Route exact path="/dinks/:id/in-progress" component={ RecipeInProgress } />
       <Route exact path="/" component={ Login } />
       <Route path="/profile" component={ Profile } />
       <Route path="/done-recipes" component={ DoneRecipes } />
       <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       <Route path="/drinks/:drinksId" component={ RecipeDetails } />
       <Route path="/meals/:mealsId" component={ RecipeDetails } />
-      <Route exact path="/drinks" component={ Recipes } />
-      <Route exact path="/meals" component={ Recipes } />
     </Switch>
   );
 }
